ec127284479e0373d29230035b042fa8526b4760
Simon Albani <simon@albani-mail.de>
2017-06-22 16:10:47 +0200

    added annotation and name to dtt

--- src/app/components/-tests/digit-triple-test/digit-triple-test.component.ts
+++ src/app/components/-tests/digit-triple-test/digit-triple-test.component.ts
@@ -4,6 +4,8 @@ import { DigitTripleTestState } from '../../../enums/DigitTripleTestState.enum';
 import { DttApiService } from '../../../services/dtt-api.service';
 import { AudioService } from '../../../services/audio.service';
 
+import State = DigitTripleTestState;
+
 @Component({
   selector: 'snscg-digit-triple-test',
   templateUrl: './digit-triple-test.component.html',
@@ -13,11 +15,16 @@ export class DigitTripleTestComponent implements OnInit, OnDestroy {
 
   activeKey = '';
 
-  state: DigitTripleTestState = DigitTripleTestState.void;
+  state: State = State.void;
   enteredNumber = '';
 
   id: string;
   progress = 0;
+
+  name = '';
+  annotation = '';
+
+  result = 0;
 
   constructor(public _languageService: LanguageService, private api: DttApiService, private audio: AudioService, private zone: NgZone) { }
 
@@ -33,56 +40,66 @@ export class DigitTripleTestComponent implements OnInit, OnDestroy {
       this.keyDownEventListener);
     window.addEventListener('keyup',
       this.keyUpEventListener);
-    if (this.id) {
-      this.api.end(this.id);
+    if (this.id && this.state <= State.finishing) {
+      this.api.finish({ id: this.id, annotation: 'test got destroyed' });
     }
   }
 
+  init() {
+    this.state = State.started;
+  }
+
   async start() {
-    this.state = DigitTripleTestState.started;
     let res = await this.api.initialize({
       language: 'de',
-      name: 'Hannes'
+      name: this.name
     });
     this.id = res.Id;
     this.present(res.TripleBuffer);
     this.audio.onMainSourceEnded.subscribe(() => {
-      this.zone.run(() => { this.state = DigitTripleTestState.input; });
+      this.zone.run(() => { this.state = State.input; });
     });
   }
 
   async continue() {
     if (this.enteredNumber.length == 3) {
+      this.state = State.validation;
       let selectedTriple = this.enteredNumber;
       this.enteredNumber = '';
       let res = await this.api.next({ id: this.id, selectedTriple: selectedTriple });
       this.progress = res.Progress;
-      this.present(res.TripleBuffer);
+      if (res.End)
+        this.state = State.finishing;
+      else
+        this.present(res.TripleBuffer);
     }
   }
 
-  async cancel() {
-    this.state = DigitTripleTestState.canceled;
+  cancel() {
+    this.state = State.finishing;
+  }
+
+  async finish() {
     this.audio.stop();
-    let res = await this.api.end(this.id);
-    if (res == 'success')
-      this.reset();
+    let res = await this.api.finish({ id: this.id, annotation: this.annotation });
+    this.result = res.Snr;
+    this.state = State.result;
   }
 
   reset() {
-    this.state = DigitTripleTestState.void;
+    this.state = State.void;
     this.enteredNumber = '';
 
     this.id = '';
   }
 
   present(data: ArrayBuffer | string) {
-    this.state = DigitTripleTestState.presentation;
+    this.state = State.presentation;
     this.audio.play(data);
   }
 
   keyDownEventListener = (e: KeyboardEvent) => {
-    if (this.state == DigitTripleTestState.input) {
+    if (this.state == State.input) {
       if (e.key.match('[0-9]')) {
         this.addNumber(e.key);
       } else if (e.key.match('Delete|Backspace'))
