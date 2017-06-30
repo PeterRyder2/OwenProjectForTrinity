2cc0aac9d81593d937939a5d1d8c2c679c676a6f
Marc Manstein-Klein <mama@hdh1>
2017-06-30 11:58:35 +0200

    new apiAdress

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
-  apiAdress = 'http://192.168.99.158:8080';
+  apiAdress = 'http://192.168.99.41:8085';
 
   constructor() { }
 
