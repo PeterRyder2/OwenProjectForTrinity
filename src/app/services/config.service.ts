11cee2477c1eacbc72d104d8545106c10c39acc4
Marc Manstein-Klein <mama@hdh1>
2017-07-04 14:29:46 +0200

    config API-IP

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
+  // apiAdress = 'http://localhost:8080';
   apiAdress = 'http://192.168.99.41:8085';
 
   constructor() { }
