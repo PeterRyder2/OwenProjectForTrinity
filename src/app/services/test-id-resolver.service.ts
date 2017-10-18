a000d6c3a13e986d99de80770aa4ba0c9afee24c
Simon_Al <simon@albani-mail.de>
2017-10-19 00:03:11 +0200

    fixed generating and language in navbar

--- src/app/services/test-id-resolver.service.ts
+++ src/app/services/test-id-resolver.service.ts
@@ -11,7 +11,6 @@ export class TestIdResolver implements Resolve<void> {
   constructor(private router: Router, private idService: IdService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): void {
-    console.log('Aa', this.idService.isIdSet)
     if (!this.idService.isIdSet)
       this.router.navigate(['/identification', route.routeConfig.path]);
   }
