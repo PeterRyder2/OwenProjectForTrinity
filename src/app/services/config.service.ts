45a0d5e05f4d162c802087ad4dc311c66250bee4
Daniel Berg <beda@hdh1>
2017-10-24 15:49:23 +0200

    external API-Server

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -5,8 +5,8 @@ export class ConfigService {
 
   // apiAdress = 'http://localhost:8085';
   // apiAdress = 'http://192.168.99.41:8085';
-  apiAdress = 'http://192.168.99.41:8085';
-  // apiAdress = 'http://82.165.69.131';
+  // apiAdress = 'http://192.168.99.41:8085';
+   apiAdress = 'http://82.165.69.131';
   // apiAdress = 'http://82.165.161.242:8085';
 
   constructor() { } 
