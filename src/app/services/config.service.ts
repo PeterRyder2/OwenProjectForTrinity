aeb717aa6bc7aa5b3a8acac10b7950ccb732f73e
Thomas Wittkop <with@hdh1>
2017-12-11 15:59:52 +0100

    .

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -3,11 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
-  apiAdress = 'http://localhost:8085';
+  apiAdress = 'http://82.165.69.131';
+  // apiAdress = 'http://localhost:80';
   // apiAdress = 'http://192.168.99.41:8085';
-  // apiAdress = 'http://192.168.99.41:8085';
-  //  apiAdress = 'http://82.165.69.131';
-  // apiAdress = 'http://82.165.161.242:8085';
-
+ 
   constructor() { } 
 }
